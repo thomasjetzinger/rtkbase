c05985d6c18a93971d272021e30f7653d2f81355
Thomas Jetzinger <t.jetzinger@gmail.com>
2020-09-17 14:04:49 +0200

    add rtk base repo for install

--- tools/install.sh
+++ tools/install.sh
@@ -152,7 +152,7 @@ install_rtklib() {
 
 rtkbase_repo(){
     #Get rtkbase repository
-    sudo -u $(logname) git clone https://github.com/stefal/rtkbase.git
+    sudo -u $(logname) git clone https://github.com/thomasjetzinger/rtkbase.git
     sudo -u $(logname) touch rtkbase/settings.conf
     add_rtkbase_path_to_environment
 
